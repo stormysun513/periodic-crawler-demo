ab7f3766be37ac7fbea156c299ef05bf859ed0a6
Yu-Lun Tsai <stormysun513@hotmail.com>
2018-04-13 00:32:22 -0700

    fix typo in start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "*/5 * * * * source $DIR/venv/bin/activate && python $DIR/crawler.sh >> /tmp/crawler.log" | env EDITOR=vim crontab
+echo "*/5 * * * * source $DIR/venv/bin/activate && python $DIR/crawler.py >> /tmp/crawler.log" | env EDITOR=vim crontab
